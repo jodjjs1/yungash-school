4d7227b1d3aa26d59f95850832abc968ae81c488
jodjjs1 <jodjjs1@gmail.com>
2021-07-21 04:04:01 +0300

    sidebar beta.2

--- scripts/sidebarScript.js
+++ scripts/sidebarScript.js
@@ -1,66 +1,35 @@
-function openSbMenu(menuPtId) {
-    let menuPt = document.querySelector('.main-pts').querySelector('#'+menuPtId);
-    let SbMenu = menuPt.querySelector('.submenu');
-    //console.log(menuPt.querySelector('.submenu'));
-    SbMenu.style.display = 'block';
-}
-
-function closeSbMenu(menuPtId) {
-    let menuPt = document.querySelector('.main-pts').querySelector('#'+menuPtId);
-    let SbMenu = menuPt.querySelector('.submenu');
-    //console.log(menuPt);
-    SbMenu.style.display = 'none';
-}
+document.querySelector('.side-main').style.height = '2000px';
 
 document.addEventListener("DOMContentLoaded", function(){
 
     document.querySelectorAll('.sidebar .mpt').forEach(function(element){
-        
-        let isOpen = false;
-
+    
         element.addEventListener('click', function (e) {
+    
+          let nextEl = element.nextElementSibling;
+          let parentEl  = element.parentElement;	
 
-            let nextEl = element.nextElementSibling;
-            let parentEl  = element.parentElement;	
-
-            console.log(element);
+          console.log(nextEl);
+          
+          let sm = element.querySelector('.submenu');
+    
+          if(nextEl) {
+            e.preventDefault();	
+            let mycollapse = new bootstrap.Collapse(nextEl);
             
-            //parentEl.parentElement.parentElement.style.height = '500px';
-            
-
-
-            if(!isOpen) {
-                openSbMenu(parentEl.id);
-                isOpen = true;
-                // parentEl.parentElement.querySelector('#nav')
+            if(nextEl.classList.contains('show')){
+              mycollapse.hide();
             } else {
-                closeSbMenu(parentEl.id);
-                //parentEl.classList.remove('open');
-                isOpen = false;
+                mycollapse.show();
+                // find other submenus with class=show
+                var opened_submenu = parentEl.parentElement.querySelector('.submenu.show');
+                // if it exists, then close all of them
+                if(opened_submenu){
+                  new bootstrap.Collapse(opened_submenu);
+                }
             }
-            console.log(parentEl.parentElement.parentElement);
-            parentEl.parentElement.parentElement.style.height = parentEl.parentElement.offsetHeight + 'px';
-            //parentEl.firstChild.style.transform = 'rotate(90deg)';
-
-
-
-                // if(nextEl) {
-                //     e.preventDefault();	
-                //     let mycollapse = new bootstrap.Collapse(nextEl);
-                    
-                //     if(nextEl.classList.contains('show')){
-                //         mycollapse.hide();
-                //     } else {
-                //         mycollapse.show();
-                //         // find other submenus with class=show
-                //         var opened_submenu = parentEl.parentElement.querySelector('.submenu.show');
-                //         // if it exists, then close all of them
-                //         if(opened_submenu){
-                //             new bootstrap.Collapse(opened_submenu);
-                //         }
-                //     }
-                // }
+        }
         }); // addEventListener
-    }) // forEach
+      }) // forEach
     }); 
     
