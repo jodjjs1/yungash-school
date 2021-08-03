637ad53867515c1bf867f9125f40811d2ed20641
jodjjs1 <jodjjs1@gmail.com>
2021-08-03 20:34:12 +0300

    add content to sidebar

--- scripts/sidebarScript.js
+++ scripts/sidebarScript.js
@@ -1,4 +1,3 @@
-document.querySelector('.side-main').style.height = '2000px';
 
 document.addEventListener("DOMContentLoaded", function(){
 
@@ -6,30 +5,29 @@ document.addEventListener("DOMContentLoaded", function(){
     
         element.addEventListener('click', function (e) {
     
-          let nextEl = element.nextElementSibling;
-          let parentEl  = element.parentElement;	
+            let nextEl = element.nextElementSibling;
+            let parentEl  = element.parentElement;	
 
-          console.log(nextEl);
-          
-          let sm = element.querySelector('.submenu');
-    
-          if(nextEl) {
-            e.preventDefault();	
-            let mycollapse = new bootstrap.Collapse(nextEl);
+            console.log(nextEl);
             
-            if(nextEl.classList.contains('show')){
-              mycollapse.hide();
-            } else {
-                mycollapse.show();
-                // find other submenus with class=show
-                var opened_submenu = parentEl.parentElement.querySelector('.submenu.show');
-                // if it exists, then close all of them
-                if(opened_submenu){
-                  new bootstrap.Collapse(opened_submenu);
+            let sm = element.querySelector('.submenu');
+        
+            if(nextEl) {
+                e.preventDefault();	
+                let mycollapse = new bootstrap.Collapse(nextEl);
+                
+                if(nextEl.classList.contains('show')){
+                mycollapse.hide();
+                } else {
+                    mycollapse.show();
+                    // find other submenus with class=show
+                    var opened_submenu = parentEl.parentElement.querySelector('.submenu.show');
+                    // if it exists, then close all of them
+                    if(opened_submenu){
+                    new bootstrap.Collapse(opened_submenu);
+                    }
                 }
             }
-        }
         }); // addEventListener
-      }) // forEach
-    }); 
-    
+    }) // forEach
+}); 
